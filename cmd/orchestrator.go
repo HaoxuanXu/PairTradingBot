f3851f79893c20b6780d442b7abbd0140f57511e
John Xu <haoxuanxu@uchicago.edu>
2021-12-19 21:20:45 -0500

    add flags for command line arguments

--- cmd/orchestrator.go
+++ cmd/orchestrator.go
@@ -1,21 +1,25 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"time"
 
 	"github.com/go-co-op/gocron"
 )
 
 func main() {
-	timeString := "9:40"
+	assetType := flag.String("assettype", "gold", "this is the asset type we will run the job with")
+	accountType := flag.String("accounttype", "paper", "This determines if we will run the job on paper or live accounts")
+	startTime := flag.String("starttime", "9:40", "this is the time we will start trading each day")
+	flag.Parse()
+
 	loc, _ := time.LoadLocation("America/New_York")
 	s := gocron.NewScheduler(loc)
-	s.Every(1).Days().At(timeString).Do(func() {
-		JobRunner(os.Args[1])
+	s.Every(1).Days().At(*startTime).Do(func() {
+		JobRunner(*assetType, *accountType)
 	})
-	fmt.Printf("Planning to run the %s job ...\n", os.Args[1])
-	fmt.Printf("Wait for the job to begin at %s AM EST\n", timeString)
+	fmt.Printf("Planning to run the %s job on the %s account...\n", *assetType, *accountType)
+	fmt.Printf("Wait for the job to begin at %s EST\n", *startTime)
 	s.StartBlocking()
 }
