8b207c8aee4dd116696d6685e197fa5de831ce4f
John Xu <haoxuanxu@uchicago.edu>
2021-12-19 21:33:23 -0500

    add entry percent as a command line argument

--- cmd/orchestrator.go
+++ cmd/orchestrator.go
@@ -12,14 +12,15 @@ func main() {
 	assetType := flag.String("assettype", "gold", "this is the asset type we will run the job with")
 	accountType := flag.String("accounttype", "paper", "This determines if we will run the job on paper or live accounts")
 	startTime := flag.String("starttime", "9:40", "this is the time we will start trading each day")
+	entryPercent := flag.Float64("entrypercent", 0.12, "this is the percent of portfolio value we will commit")
 	flag.Parse()
 
 	loc, _ := time.LoadLocation("America/New_York")
 	s := gocron.NewScheduler(loc)
 	s.Every(1).Days().At(*startTime).Do(func() {
-		JobRunner(*assetType, *accountType)
+		JobRunner(*assetType, *accountType, *entryPercent)
 	})
-	fmt.Printf("Planning to run the %s job on the %s account...\n", *assetType, *accountType)
+	fmt.Printf("Planning to run the %s job for %.1f%% of the portfolio on the %s account...\n", *assetType, *entryPercent*100, *accountType)
 	fmt.Printf("Wait for the job to begin at %s EST\n", *startTime)
 	s.StartBlocking()
 }
--- cmd/runner.go
+++ cmd/runner.go
@@ -2,6 +2,6 @@ package main
 
 import "github.com/HaoxuanXu/TradingBot/strats/pairtrading"
 
-func JobRunner(assetType, accountType string) {
-	pairtrading.PairTradingJob(assetType, accountType, 0.8)
+func JobRunner(assetType, accountType string, entryPercent float64) {
+	pairtrading.PairTradingJob(assetType, accountType, entryPercent)
 }
