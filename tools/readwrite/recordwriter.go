fdac237cdbbeb68039ea4cfe7fbffda33ad9861e
John Xu <haoxuanxu@uchicago.edu>
2021-12-22 18:35:00 -0500

    change recursive error handling to iterative error handling

--- tools/readwrite/recordwriter.go
+++ tools/readwrite/recordwriter.go
@@ -9,21 +9,23 @@ import (
 func WriteIntSlice(sli []int, path string) {
 	recordBytes, errMarshal := json.Marshal(sli)
 	errWrite := ioutil.WriteFile(path, recordBytes, 0644)
-	if errWrite != nil || errMarshal != nil {
+	for errWrite != nil || errMarshal != nil {
 		log.Printf("error in serializing: %s\n", errMarshal)
 		log.Printf("error in writing to disk: %s\n", errWrite)
 		log.Println("Rewriting ...")
-		WriteIntSlice(sli, path)
+		recordBytes, errMarshal = json.Marshal(sli)
+		errWrite = ioutil.WriteFile(path, recordBytes, 0644)
 	}
 }
 
 func WriteFloatSlice(sli []float64, path string) {
 	recordBytes, errMarshal := json.Marshal(sli)
 	errWrite := ioutil.WriteFile(path, recordBytes, 0644)
-	if errMarshal != nil || errWrite != nil {
+	for errMarshal != nil || errWrite != nil {
 		log.Printf("error in serializing: %s\n", errMarshal)
 		log.Printf("error in writing to disk: %s\n", errWrite)
 		log.Println("Rewriting ...")
-		WriteFloatSlice(sli, path)
+		recordBytes, errMarshal = json.Marshal(sli)
+		errWrite = ioutil.WriteFile(path, recordBytes, 0644)
 	}
 }
