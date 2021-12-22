1aac4993180dd015f532394403763162dd00a4e9
John Xu <haoxuanxu@uchicago.edu>
2021-12-22 17:02:34 -0500

    remove transaction functions in the job file

--- strats/pairtrading/job.go
+++ strats/pairtrading/job.go
@@ -12,7 +12,6 @@ import (
 	"github.com/HaoxuanXu/TradingBot/strats/pairtrading/pipeline"
 	"github.com/HaoxuanXu/TradingBot/strats/pairtrading/quotesprocessor"
 	"github.com/HaoxuanXu/TradingBot/strats/pairtrading/signalcatcher"
-	"github.com/HaoxuanXu/TradingBot/strats/pairtrading/transaction"
 	"github.com/HaoxuanXu/TradingBot/tools/util"
 )
 
@@ -122,7 +121,6 @@ func PairTradingJob(assetType, accountType string, entryPercent float64) {
 
 	// Close all positions and record data
 	tradingBroker.CloseAllPositions()
-	transaction.SlideRepeatAndPriceRatioArrays(dataModel)
 	log.Printf("The amount you made today: $%.2f\n", tradingBroker.GetDailyProfit())
 	log.Printf("The number of round trips you made today: %d\n", tradingBroker.TransactionNums)
 	log.Printf("The number of losing trips you made todau: %d\n", dataModel.LoserNums)
