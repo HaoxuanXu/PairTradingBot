2a0fde567a36c4f09dbf34272fb759489d599f81
John Xu <haoxuanxu@uchicago.edu>
2021-12-22 18:26:35 -0500

    add recursive error handling for writing operation

--- tools/readwrite/recordwriter.go
+++ tools/readwrite/recordwriter.go
@@ -3,14 +3,27 @@ package readwrite
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 )
 
 func WriteIntSlice(sli []int, path string) {
-	recordBytes, _ := json.Marshal(sli)
-	ioutil.WriteFile(path, recordBytes, 0644)
+	recordBytes, errMarshal := json.Marshal(sli)
+	errWrite := ioutil.WriteFile(path, recordBytes, 0644)
+	if errWrite != nil || errMarshal != nil {
+		log.Printf("error in serializing: %s\n", errMarshal)
+		log.Printf("error in writing to disk: %s\n", errWrite)
+		log.Println("Rewriting ...")
+		WriteIntSlice(sli, path)
+	}
 }
 
 func WriteFloatSlice(sli []float64, path string) {
-	recordBytes, _ := json.Marshal(sli)
-	ioutil.WriteFile(path, recordBytes, 0644)
+	recordBytes, errMarshal := json.Marshal(sli)
+	errWrite := ioutil.WriteFile(path, recordBytes, 0644)
+	if errMarshal != nil || errWrite != nil {
+		log.Printf("error in serializing: %s\n", errMarshal)
+		log.Printf("error in writing to disk: %s\n", errWrite)
+		log.Println("Rewriting ...")
+		WriteFloatSlice(sli, path)
+	}
 }
