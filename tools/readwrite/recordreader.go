f681153e63e789ddafc6194bf7498d96f1d6dc61
John Xu <haoxuanxu@uchicago.edu>
2021-12-22 19:56:36 -0500

    add error handling for reader functions

--- tools/readwrite/recordreader.go
+++ tools/readwrite/recordreader.go
@@ -3,13 +3,22 @@ package readwrite
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 )
 
 func ReadRecordInt(path string) []int {
 
 	var recordContainer []int
-	recordBytes, _ := ioutil.ReadFile(path)
-	json.Unmarshal(recordBytes, &recordContainer)
+	recordBytes, errReadBytes := ioutil.ReadFile(path)
+	errUnmarshal := json.Unmarshal(recordBytes, &recordContainer)
+
+	for errReadBytes != nil || errUnmarshal != nil {
+		log.Printf("Error in unmarshalling for int slice: %s\n", errUnmarshal.Error())
+		log.Printf("Error in errReadBytes for int slice: %s\n", errReadBytes.Error())
+		log.Println("Re-reading ...")
+		recordBytes, errReadBytes = ioutil.ReadFile(path)
+		errUnmarshal = json.Unmarshal(recordBytes, &recordContainer)
+	}
 
 	return recordContainer
 }
@@ -17,8 +26,16 @@ func ReadRecordInt(path string) []int {
 func ReadRecordFloat(path string) []float64 {
 
 	var recordContainer []float64
-	recordBytes, _ := ioutil.ReadFile(path)
-	json.Unmarshal(recordBytes, &recordContainer)
+	recordBytes, errReadBytes := ioutil.ReadFile(path)
+	errUnmarshal := json.Unmarshal(recordBytes, &recordContainer)
+
+	for errReadBytes != nil || errUnmarshal != nil {
+		log.Printf("Error in unmarshalling for float slice: %s\n", errUnmarshal.Error())
+		log.Printf("Error in errReadBytes for float slice: %s\n", errReadBytes.Error())
+		log.Println("Re-reading ...")
+		recordBytes, errReadBytes = ioutil.ReadFile(path)
+		errUnmarshal = json.Unmarshal(recordBytes, &recordContainer)
+	}
 
 	return recordContainer
 }
--- tools/readwrite/recordwriter.go
+++ tools/readwrite/recordwriter.go
@@ -10,8 +10,8 @@ func WriteIntSlice(sli []int, path string) {
 	recordBytes, errMarshal := json.Marshal(sli)
 	errWrite := ioutil.WriteFile(path, recordBytes, 0644)
 	for errWrite != nil || errMarshal != nil {
-		log.Printf("error in serializing: %s\n", errMarshal)
-		log.Printf("error in writing to disk: %s\n", errWrite)
+		log.Printf("error in marshalling for int slice: %s\n", errMarshal.Error())
+		log.Printf("error in writing to disk for int slice: %s\n", errWrite.Error())
 		log.Println("Rewriting ...")
 		recordBytes, errMarshal = json.Marshal(sli)
 		errWrite = ioutil.WriteFile(path, recordBytes, 0644)
@@ -22,8 +22,8 @@ func WriteFloatSlice(sli []float64, path string) {
 	recordBytes, errMarshal := json.Marshal(sli)
 	errWrite := ioutil.WriteFile(path, recordBytes, 0644)
 	for errMarshal != nil || errWrite != nil {
-		log.Printf("error in serializing: %s\n", errMarshal)
-		log.Printf("error in writing to disk: %s\n", errWrite)
+		log.Printf("error in marshalling for float slice: %s\n", errMarshal.Error())
+		log.Printf("error in writing to disk for float slice: %s\n", errWrite.Error())
 		log.Println("Rewriting ...")
 		recordBytes, errMarshal = json.Marshal(sli)
 		errWrite = ioutil.WriteFile(path, recordBytes, 0644)
