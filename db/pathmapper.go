aad0fbd932438d3a8f934ad8dc34943769cea810
Haoxuan Xu <haoxuanxu@uchicago.edu>
2022-01-21 21:03:13 -0500

    update change from prod

--- db/pathmapper.go
+++ db/pathmapper.go
@@ -5,7 +5,7 @@ const (
 	shortGLDLongIAU = "./db/pairtrading/gold/price_ratio/short_gld_long_iau.json"
 	goldRepeatNums  = "./db/pairtrading/gold/repeat_num/price_ratio_num_repeat.json"
 	goldLogPath     = "./db/pairtrading/gold/log/"
-	monitorLogPath  = "/var/log/"
+	monitorLogPath  = "/home/pi/TradingBotLogs"
 )
 
 func MapRecordPath(strat string) (string, string, string) {
--- tools/logging/logging.go
+++ tools/logging/logging.go
@@ -14,16 +14,11 @@ func SetLogging(assetType string) *os.File {
 	dt := time.Now()
 	logName := fmt.Sprintf("%d-%d-%d_%s_TradingLog.log", dt.Year(), dt.Month(), dt.Day(), assetType)
 	fullLogPath := db.MapLogPath(assetType) + logName
-	monitorLogPath := db.MapLogPath("monitor") + "tradingbot.log"
 	logFile, err := os.OpenFile(fullLogPath, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	monitorLogFile, err := os.OpenFile(monitorLogPath, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
-	if err != nil {
-		log.Fatal(err)
-	}
-	multiWrite := io.MultiWriter(logFile, monitorLogFile)
+	multiWrite := io.MultiWriter(logFile)
 	log.SetOutput(multiWrite)
 	log.Printf("logging the trading record to %s\n", fullLogPath)
 	return logFile
