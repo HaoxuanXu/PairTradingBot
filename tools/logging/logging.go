344bdc66958461d924cedcbf457fd23c7bfe462f
John Xu <haoxuanxu@uchicago.edu>
2022-01-29 20:53:39 -0500

    add monitor log

--- tools/logging/logging.go
+++ tools/logging/logging.go
@@ -14,11 +14,16 @@ func SetLogging(assetType string) *os.File {
 	dt := time.Now()
 	logName := fmt.Sprintf("%d-%d-%d_%s_TradingLog.log", dt.Year(), dt.Month(), dt.Day(), assetType)
 	fullLogPath := db.MapLogPath(assetType) + logName
+	monitorLogPath := db.MapLogPath("monitor") + "TradingLog.log"
 	logFile, err := os.OpenFile(fullLogPath, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
-	multiWrite := io.MultiWriter(logFile)
+	monitorLog, err := os.OpenFile(monitorLogPath, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	multiWrite := io.MultiWriter(logFile, monitorLog)
 	log.SetOutput(multiWrite)
 	log.Printf("logging the trading record to %s\n", fullLogPath)
 	return logFile
