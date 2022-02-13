0029e57e6ed1b7d59b7e4d4fb876e70cc82c279d
John Xu <haoxuanxu@uchicago.edu>
2022-02-13 10:43:19 -0500

    assume the transaction fails if repeatnum is 1 more than the threshold

--- tools/repeater/repeater.go
+++ tools/repeater/repeater.go
@@ -11,7 +11,7 @@ func calculateHypotheticalProfit(repeatMapper map[int]int, repeatNum int) float6
 	for key, val := range repeatMapper {
 		if key < repeatNum {
 			continue
-		} else if key == repeatNum {
+		} else if key == repeatNum || key == repeatNum+1 {
 			profit -= 5.0 * float64(val)
 		} else {
 			profit += float64(val)
