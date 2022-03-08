a172fecf8e171a0341e7c521b6f323c633003251
John Xu <haoxuanxu@uchicago.edu>
2022-03-08 13:35:05 -0500

    debug

--- db/pathmapper.go
+++ db/pathmapper.go
@@ -8,6 +8,11 @@ const (
 	shortGLDLongIAU           = priceRatioPath + "short_gld_long_iau.json"
 	longGLDShortIAURepeatNums = repeatNumPath + "long_gld_short_iau_num_repeat.json"
 	shortGLDLongIAURepeatNums = repeatNumPath + "short_gld_long_iau_num_repeat.json"
+
+	longAGGShortBND           = priceRatioPath + "long_agg_short_bnd.json"
+	shortAGGLongBND           = priceRatioPath + "short_agg_long_bnd.json"
+	longAGGShortBNDRepeatNums = repeatNumPath + "long_agg_short_bnd_num_repeat.json"
+	shortAGGLongBNDRepeatNums = repeatNumPath + "short_agg_long_bnd_num_repeat.json"
 	logPath                   = "./db/pairtrading/log/"
 
 	monitorLogPath = "./"
@@ -28,6 +33,11 @@ func getAssetParamConfig(strat string) *AssetParamConfig {
 		assetParamConfig.LongExpensiveShortCheapPriceRatioPath = longGLDShortIAU
 		assetParamConfig.ShortExpensiveLongCheapRepeatNumPath = shortGLDLongIAURepeatNums
 		assetParamConfig.LongExpensiveShortCheapRepeatNumPath = longGLDShortIAURepeatNums
+	} else if strat == "bond" {
+		assetParamConfig.ShortExensiveLongCheapPriceRatioPath = shortAGGLongBND
+		assetParamConfig.LongExpensiveShortCheapPriceRatioPath = longAGGShortBND
+		assetParamConfig.ShortExpensiveLongCheapRepeatNumPath = shortAGGLongBNDRepeatNums
+		assetParamConfig.LongExpensiveShortCheapRepeatNumPath = longAGGShortBNDRepeatNums
 	}
 	return assetParamConfig
 }
