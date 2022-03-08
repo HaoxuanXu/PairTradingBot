6a7bb355bedda1a4a0d33c83f7c3293c53a7e1af
John Xu <haoxuanxu@uchicago.edu>
2022-03-08 13:47:14 -0500

    debug

--- tools/readwrite/recordwriter.go
+++ tools/readwrite/recordwriter.go
@@ -11,9 +11,9 @@ func WriteIntSlice(sli *[]int, path string) {
 	errWrite := ioutil.WriteFile(path, recordBytes, 0644)
 	for errWrite != nil || errMarshal != nil {
 		if errMarshal != nil {
-			log.Printf("error in marshalling for float slice: %s\n", errMarshal.Error())
+			log.Printf("error in marshalling for int slice: %s\n", errMarshal.Error())
 		} else if errWrite != nil {
-			log.Printf("error in writing to disk for float slice: %s\n", errWrite.Error())
+			log.Printf("error in writing to disk for int slice: %s\n", errWrite.Error())
 			log.Printf("path: %s\n", path)
 		}
 		log.Println("Rewriting ...")
@@ -30,6 +30,7 @@ func WriteFloatSlice(sli *[]float64, path string) {
 			log.Printf("error in marshalling for float slice: %s\n", errMarshal.Error())
 		} else if errWrite != nil {
 			log.Printf("error in writing to disk for float slice: %s\n", errWrite.Error())
+			log.Printf("path: %s\n", path)
 		}
 		log.Println("Rewriting ...")
 		recordBytes, errMarshal = json.Marshal(sli)
