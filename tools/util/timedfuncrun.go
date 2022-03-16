70159459e23dc94e619ba602e102075b33fa94ff
John Xu <haoxuanxu@uchicago.edu>
2022-03-16 11:18:41 -0400

    not have interval

--- tools/util/timedfuncrun.go
+++ tools/util/timedfuncrun.go
@@ -4,10 +4,9 @@ import (
 	"time"
 )
 
-func TimedFuncRun(runDuration time.Duration, runFunc func(), interval time.Duration) {
+func TimedFuncRun(runDuration time.Duration, runFunc func()) {
 	beginTime := time.Now()
 	for time.Since(beginTime) <= runDuration {
 		runFunc()
-		time.Sleep(interval)
 	}
 }
