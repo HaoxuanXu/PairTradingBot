4a9902101184a6f51da021c46d6191196fc32bc3
John Xu <haoxuanxu@uchicago.edu>
2022-03-29 12:12:06 -0400

    use average

--- strats/pairtrading/model/pair_trading_model.go
+++ strats/pairtrading/model/pair_trading_model.go
@@ -4,6 +4,7 @@ import (
 	"github.com/HaoxuanXu/TradingBot/strats/pairtrading/updater"
 	"github.com/HaoxuanXu/TradingBot/tools/readwrite"
 	"github.com/HaoxuanXu/TradingBot/tools/repeater"
+	"github.com/HaoxuanXu/TradingBot/tools/util"
 	"github.com/alpacahq/alpaca-trade-api-go/v2/alpaca"
 )
 
@@ -123,8 +124,18 @@ func (model *PairTradingModel) UpdateParameters() {
 		model.LongExpensiveStockShortCheapStockPriceRatioRecord,
 		model.ShortExpensiveStockLongCheapStockPriceRatioRecord,
 	)
-	model.LongExpensiveShortCheapRepeatNumThreshold = repeater.CalculateOptimalRepeatNum(model.LongExpensiveShortCheapRepeatArray)
-	model.ShortExpensiveLongCheapRepeatNumThreshold = repeater.CalculateOptimalRepeatNum(model.ShortExpensiveLongCheapRepeatArray)
+	model.LongExpensiveShortCheapRepeatNumThreshold = util.GetAvgInt(
+		[]int{
+			repeater.CalculateOptimalRepeatNum(model.LongExpensiveShortCheapRepeatArray),
+			repeater.CalculateOptimalRepeatNum(model.LongExpensiveShortCheapRepeatArray[len(model.LongExpensiveShortCheapRepeatArray)/2:]),
+		},
+	)
+	model.ShortExpensiveLongCheapRepeatNumThreshold = util.GetAvgInt(
+		[]int{
+			repeater.CalculateOptimalRepeatNum(model.ShortExpensiveLongCheapRepeatArray),
+			repeater.CalculateOptimalRepeatNum(model.ShortExpensiveLongCheapRepeatArray[len(model.ShortExpensiveLongCheapRepeatArray)/2:]),
+		},
+	)
 }
 
 func (model *PairTradingModel) ClearRepeatNumber() {
