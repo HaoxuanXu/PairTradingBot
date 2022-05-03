40fe5f0988ea95f9eef433d7b41ec25c1d0abf1c
John Xu <haoxuanxu@uchicago.edu>
2022-05-03 13:23:22 -0400

    reimplement volatility

--- strats/pairtrading/signalcatcher/signalcatcher.go
+++ strats/pairtrading/signalcatcher/signalcatcher.go
@@ -9,12 +9,14 @@ func GetEntrySignal(shortExpensiveStock bool, model *model.PairTradingModel, bro
 	if !broker.HasPosition {
 		if shortExpensiveStock {
 			if model.ShortExpensiveStockLongCheapStockPriceRatio > model.PriceRatioThreshold &&
-				model.ShortExpensiveStockLongCheapStockRepeatNumber >= model.ShortExpensiveLongCheapRepeatNumThreshold {
+				model.ShortExpensiveStockLongCheapStockRepeatNumber >= model.ShortExpensiveLongCheapRepeatNumThreshold &&
+				model.CurrentPriceVolatility <= model.AvgPriceVolatility {
 				return true
 			}
 		} else {
 			if model.LongExpensiveStockShortCheapStockPriceRatio < model.PriceRatioThreshold &&
-				model.LongExpensiveStockShortCheapStockRepeatNumber >= model.LongExpensiveShortCheapRepeatNumThreshold {
+				model.LongExpensiveStockShortCheapStockRepeatNumber >= model.LongExpensiveShortCheapRepeatNumThreshold &&
+				model.CurrentPriceVolatility <= model.AvgPriceVolatility {
 				return true
 			}
 		}
@@ -24,13 +26,15 @@ func GetEntrySignal(shortExpensiveStock bool, model *model.PairTradingModel, bro
 
 func GetExitSignal(model *model.PairTradingModel) bool {
 	if model.IsShortExpensiveStockLongCheapStock &&
-		model.LongExpensiveStockShortCheapStockRepeatNumber >= model.LongExpensiveShortCheapRepeatNumThreshold {
+		model.LongExpensiveStockShortCheapStockRepeatNumber >= model.LongExpensiveShortCheapRepeatNumThreshold &&
+		model.CurrentPriceVolatility <= model.AvgPriceVolatility {
 		model.ExitNetValue = model.CheapStockShortQuotePrice*model.CheapStockEntryVolume - model.ExpensiveStockLongQuotePrice*model.ExpensiveStockEntryVolume
 		if model.ExitNetValue+model.EntryNetValue >= model.MinProfitThreshold.Applied {
 			return true
 		}
 	} else if model.IsLongExpensiveStockShortCheapStock &&
-		model.ShortExpensiveStockLongCheapStockRepeatNumber >= model.ShortExpensiveLongCheapRepeatNumThreshold {
+		model.ShortExpensiveStockLongCheapStockRepeatNumber >= model.ShortExpensiveLongCheapRepeatNumThreshold &&
+		model.CurrentPriceVolatility <= model.AvgPriceVolatility {
 		model.ExitNetValue = model.ExpensiveStockShortQuotePrice*model.ExpensiveStockEntryVolume - model.CheapStockLongQuotePrice*model.CheapStockEntryVolume
 		if model.ExitNetValue+model.EntryNetValue >= model.MinProfitThreshold.Applied {
 			return true
