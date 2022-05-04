032610814c80230ab11d9385fa60db9e352de332
John Xu <haoxuanxu@uchicago.edu>
2022-05-04 16:26:46 -0400

    remove volatility data

--- tools/repeater/repeater.go
+++ tools/repeater/repeater.go
@@ -9,7 +9,7 @@ func calculateHypotheticalProfit(repeatMapper map[int]int, repeatNum int) float6
 	for key, val := range repeatMapper {
 		if key < repeatNum {
 			continue
-		} else if key == repeatNum {
+		} else if key == repeatNum || key == repeatNum+1 {
 			profit -= 5 * float64(val)
 		} else {
 			profit += float64(val)
