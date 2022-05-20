a35d22589df250977bb4d8fdb241a82ad44158ed
John Xu <haoxuanxu@uchicago.edu>
2022-05-19 22:11:30 -0400

    modify

--- strats/pairtrading/model/pair_trading_model.go
+++ strats/pairtrading/model/pair_trading_model.go
@@ -149,7 +149,7 @@ func (model *PairTradingModel) UpdateParameters() {
 
 func (model *PairTradingModel) UpdateProfitThreshold() {
 	model.MinProfitThreshold.Low = model.CalculateMinProfitThreshold(22.9)
-	model.MinProfitThreshold.High = model.CalculateMinProfitThreshold(35.0)
+	model.MinProfitThreshold.High = model.CalculateMinProfitThreshold(30.0)
 	model.MinProfitThreshold.Applied = model.MinProfitThreshold.High
 }
 
